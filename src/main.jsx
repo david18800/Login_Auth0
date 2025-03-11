6e1ee95da1cebbc7215bc12d04943a442ddd16bd
david18800 <hippie181800@gmail.com>
2025-03-11 02:13:20 -0500

    develop

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URI;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{ redirect_uri: redirectUri }}
+      >
+        <App />
+      </Auth0Provider>,
   </StrictMode>,
 )
